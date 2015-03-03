ea591045c4ecca0676ccebbb3ead9d1ccb8de852
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2015-03-03 04:27:20 +0200

    Cache fixes

--- examples/exampleApplication/applications/example/init/test.js
+++ examples/exampleApplication/applications/example/init/test.js
@@ -1,15 +1,18 @@
-setInterval(function() {
-  console.dir({
-    //cache: Object.keys(application.cache),
-    templates: Object.keys(application.cache.templates),
-    files: Object.keys(application.cache.files),
-    folders: Object.keys(application.cache.folders),
-    scripts: Object.keys(application.cache.scripts),
-    //watchers: Object.keys(application.cache.watchers),
-    //timer: Object.keys(application.cache.timer),
-    //updates: Object.keys(application.cache.updates),
-    static: Object.keys(application.cache.static),
-    //pages: Object.keys(application.cache.pages),
-    //size: Object.keys(application.cache.size)
-  });
-}, 5000);
+if (api.cluster.isWorker) {
+  setInterval(function() {
+    console.dir({
+      //cache: Object.keys(application.cache),
+      templates: Object.keys(application.cache.templates),
+      files: application.cache.files,
+      folders: application.cache.folders,
+      scripts: Object.keys(application.cache.scripts),
+      //watchers: Object.keys(application.cache.watchers),
+      //timer: Object.keys(application.cache.timer),
+      //updates: Object.keys(application.cache.updates),
+      static: Object.keys(application.cache.static),
+      //pages: Object.keys(application.cache.pages),
+      //size: Object.keys(application.cache.size)
+    });
+  }, 5000);
+}
+
